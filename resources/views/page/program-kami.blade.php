fdab3fcae4cb9ecece63b74008e7aac333deb955
alfianizzah <alfianizzah6@gmail.com>
2021-12-05 11:58:59 +0800

    vgghb

--- resources/views/page/program-kami.blade.php
+++ resources/views/page/program-kami.blade.php
@@ -7,13 +7,17 @@
     <div class="container col-xxl-8 px-4 py-5">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div class="col-10 col-sm-8 col-lg-6">
-                <img src="{{ asset('assets/img/header3.png') }}" class="d-block mx-lg-auto img-fluid"
-                    alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+                <img src="{{ asset('assets/img/pasarakyat/economic_empowerment.png') }}"
+                    class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
             </div>
             <div class="col-lg-6">
-                <h1 class="display-5 fw-bold lh-1 mb-3">Membantu Pedagang</h1>
-                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis et
-                    exercitationem eaque libero quasi possimus porro similique ex? Magni?</p>
+                <h1 class="display-5 fw-bold lh-1 mb-3">Economic Empowerment</h1>
+                <p class="lead">
+                    Penguatan kapasitas ekonomi, kompetensi dan daya saing pelaku usaha mikro khususnya yang melakukan
+                    aktivitas ekonomi di dalam atau di sekitar pasar tradisional. Penguatan berupa peningkatan
+                    pengetahuan dan kompetensi yang relevan, mediasi dan edukasi inklusi keuangan, bantuan teknis,
+                    pendampingan, dan affirmative action lainnya.
+                </p>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-start">
                     <button class="btn btn-success"
                         style="border: 2px dashed white">https://pasarakyat.digital/</button>
@@ -26,18 +30,50 @@
             <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
 
                 <div class="col-lg-6">
-                    <h1 class="display-5 fw-bold lh-1 mb-3">Membantu Pedagang</h1>
-                    <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis
-                        et
-                        exercitationem eaque libero quasi possimus porro similique ex? Magni?</p>
+                    <h1 class="display-5 fw-bold lh-1 mb-3">Collaborative Action</h1>
+                    <p class="lead">Kerjasama program dan kegiatan dengan kementerian dan Lembaga pemerintah
+                        terkait serta Lembaga non pemerintah dan individu yang memiliki kepedulian terhadap pengembangan
+                        unit ekonomi mikro
+                    </p>
                     <div class="d-grid gap-2 d-md-flex justify-content-md-start">
                         <button class="btn btn-success"
                             style="border: 2px dashed white">https://pasarakyat.digital/</button>
                     </div>
                 </div>
                 <div class="col-10 col-sm-8 col-lg-6">
-                    <img src="{{ asset('assets/img/header2.png') }}" class="d-block mx-lg-auto img-fluid"
-                        alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+                    <img src="{{ asset('assets/img/pasarakyat/economic_collaborative_action.png') }}"
+                        class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500"
+                        loading="lazy">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="{{ asset('assets/img/pasarakyat/economic_integrated_platform.png') }}"
+                    class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold lh-1 mb-3">Integrated Platform</h1>
+                <p class="lead">
+                <ul>
+                    <li class="py-2">pasarakyat.digital, platform market place untuk memyaitu:fasilitasi
+                        transaksi antara pembeli dan
+                        pedagang pasar tradisional
+                    </li>
+                    <li class="py-2">
+                        Pedagangpasar, aplikasi bagi pedagang pasar yang terintegrasi dengan aplikasi pendukung, yaitu:
+                        Aplikasi Stok Pasar (pengelolaan stok oleh pedagang pasar), Aplikasi Dompet Pasar (pengeloaan
+                        dana oleh pedagang pasar dan umum), Aplikasi Kurir Pasar (jasa transportasi online dengan
+                        memberdayakan penduduk di sekitar pasar), Aplikasi Pasar Cerdas (forum diskusi, komunikasi, dan
+                        sharing informasi dan pengetahuan untuk pedagang pasar)
+                    </li>
+                </ul>
+                </p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button class="btn btn-success"
+                        style="border: 2px dashed white">https://pasarakyat.digital/</button>
                 </div>
             </div>
         </div>
